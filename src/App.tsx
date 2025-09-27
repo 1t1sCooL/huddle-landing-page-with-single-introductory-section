58734cd932d56d7997d63684604f69bf0bcd9cbd
1t1sCooL <mmalabugin@gmail.com>
2025-09-27 22:42:33 +0300

    update style and adaptive design

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styles from './App.module.css'
+import {Footer} from "./Footer.tsx";
+import {Icons} from "./Icons.tsx";
+import {Left} from "./Left.tsx";
+import {Right} from "./Right.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <>  <section className={styles.container}>
+        <Left/>
+        <Right/>
+    </section>
+        <Icons/>
+        <Footer/>
     </>
   )
 }
